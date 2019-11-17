86b33018983c2e5dc9f4322e4f7d022eaa754a7e
chichiben <chichi.leigh@gmail.com>
2019-11-17 16:29:19 -0500

    added firebase authentication

--- app/src/main/java/com/example/antilitter/LoginActivity.kt
+++ app/src/main/java/com/example/antilitter/LoginActivity.kt
@@ -6,18 +6,70 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 
+import android.content.Intent
+
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
+
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 // This activity displays the scores and times of users of
 // this app
 class LoginActivity : AppCompatActivity(){
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+
     private var userEmail: EditText? = null
     private var userPassword: EditText? = null
     private var loginBtn: ImageButton? = null
+
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
 
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+
         initializeUI()
+
+        loginBtn!!.setOnClickListener { loginUserAccount() }
+    }
+
+    private fun loginUserAccount() {
+        val email: String = userEmail?.text.toString()
+        val password: String = userPassword?.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(applicationContext, "Please enter email...", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(applicationContext, "Please enter password...", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        mAuth!!.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    intent.putExtra(UserID, mAuth?.currentUser?.uid)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(applicationContext, "Login failed! Please try again later", Toast.LENGTH_LONG).show()
+                }
+            }
 
     }
 
@@ -31,6 +83,12 @@ class LoginActivity : AppCompatActivity(){
     override fun onBackPressed() {
         super.onBackPressed()
         overridePendingTransition(R.anim.trans2, R.anim.out2)
+    }
+
+    companion object {
+        val UserMail = "com.example.antilitter.myhomelibrary.UMail"
+        val UserID = "com.example.antilitter.myhomelibrary.UID"
+
     }
 
 }
--- app/src/main/java/com/example/antilitter/RegistrationActivity.kt
+++ app/src/main/java/com/example/antilitter/RegistrationActivity.kt
@@ -1,10 +1,15 @@
 package com.example.antilitter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 // This activity displays the scores and times of users of
 // this app
@@ -13,12 +18,47 @@ class RegistrationActivity : AppCompatActivity(){
     private var passwordTV: EditText? = null
     private var regBtn: ImageButton? = null
 
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_screen)
 
+        mAuth = FirebaseAuth.getInstance()
+
         initializeUI()
+
+        regBtn!!.setOnClickListener { registerNewUser() }
     }
+
+    private fun registerNewUser() {
+        val email: String
+        val password: String
+        email = emailTV!!.text.toString()
+        password = passwordTV!!.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(applicationContext, "Please enter email...", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(applicationContext, "Please enter password!", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        mAuth!!.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Registration successful!", Toast.LENGTH_LONG).show()
+
+                    val intent = Intent(this@RegistrationActivity, LoginActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(applicationContext, "Registration failed! Please try again later", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
     private fun initializeUI() {
         emailTV = findViewById(R.id.email)
         passwordTV = findViewById(R.id.password)
