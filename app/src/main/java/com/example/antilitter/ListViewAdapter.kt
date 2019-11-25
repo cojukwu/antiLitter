080f96a2348eba5505ca4df6f0f95afb1c5f7e11
kelliw <kwebber1@terpmail.umd.edu>
2019-11-24 21:38:35 -0500

    no more listview

--- app/src/main/java/com/example/antilitter/ListViewAdapter.kt
+++ app/src/main/java/com/example/antilitter/ListViewAdapter.kt
@@ -10,41 +10,41 @@ import android.widget.TextView
 internal class ListViewAdapter(context: Context, resource: Int, objects: Array<String?>) :
     ArrayAdapter<String>(context, resource, objects) {
 
-    private var mLayoutInflater: LayoutInflater = LayoutInflater.from(context)
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
-        val newView: View
-
-        // Check for recycled View
-        if (null == convertView) {
-
-            // Not recycled. Create the View
-            newView = mLayoutInflater.inflate(R.layout.leaderboard_screen, parent, false)
-
-            // Cache View information in ViewHolder Object
-            val viewHolder = ViewHolder()
-            newView.tag = viewHolder
-            viewHolder.textView = newView.findViewById(R.id.rank_list_view)
-
-        } else {
-            newView = convertView
-        }
-
-        // Set the View's data
-
-        // Retrieve the viewHolder Object
-        val storedViewHolder = newView.tag as ViewHolder
-
-        //Set the data in the data View
-        storedViewHolder.textView.text = getItem(position)
-
-        return newView
-    }
-
-    // The ViewHolder class. See:
-    // http://developer.android.com/training/improving-layouts/smooth-scrolling.html#ViewHolder
-    internal class ViewHolder {
-        lateinit var textView: TextView
-    }
+//    private var mLayoutInflater: LayoutInflater = LayoutInflater.from(context)
+//
+//    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+//
+//        val newView: View
+//
+//        // Check for recycled View
+//        if (null == convertView) {
+//
+//            // Not recycled. Create the View
+//            newView = mLayoutInflater.inflate(R.layout.leaderboard_screen, parent, false)
+//
+//            // Cache View information in ViewHolder Object
+//            val viewHolder = ViewHolder()
+//            newView.tag = viewHolder
+//            viewHolder.textView = newView.findViewById(R.id.rank_list_view)
+//
+//        } else {
+//            newView = convertView
+//        }
+//
+//        // Set the View's data
+//
+//        // Retrieve the viewHolder Object
+//        val storedViewHolder = newView.tag as ViewHolder
+//
+//        //Set the data in the data View
+//        storedViewHolder.textView.text = getItem(position)
+//
+//        return newView
+//    }
+//
+//    // The ViewHolder class. See:
+//    // http://developer.android.com/training/improving-layouts/smooth-scrolling.html#ViewHolder
+//    internal class ViewHolder {
+//        lateinit var textView: TextView
+//    }
 }
